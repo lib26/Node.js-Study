712fc7355d7e08d3fa3dc2c3302fc352ce345f98
lib26 <dladlsqja@naver.com>
2022-12-29 21:59:03 +0900

    jwt 외않되

---
+++ 13-dwitter-jwt/client/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import AuthService from './service/auth';
+import TweetService from './service/tweet';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { AuthErrorEventBus } from './context/AuthContext';
+import HttpClient from './network/http';
+import TokenStorage from './db/token';
+
+const baseURL = process.env.REACT_APP_BASE_URL;
+const tokenStorage = new TokenStorage();
+const httpClient = new HttpClient(baseURL);
+const authErrorEventBus = new AuthErrorEventBus();
+const authService = new AuthService(httpClient, tokenStorage);
+const tweetService = new TweetService(httpClient, tokenStorage);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider
+        authService={authService}
+        authErrorEventBus={authErrorEventBus}
+      >
+        <App tweetService={tweetService} />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ 13-dwitter-jwt/server/router/auth.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import {} from 'express-async-errors';
+import { body } from 'express-validator';
+import { validate } from '../middleware/validator.js';
+import * as authController from '../controller/auth.js';
+import { isAuth } from '../middleware/auth.js';
+
+const router = express.Router();
+
+const validateCredential = [
+  body('username')
+    .trim()
+    .notEmpty()
+    .withMessage('username should be at least 5 characters'),
+  body('password')
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage('password should be at least 5 characters'),
+  validate,
+];
+
+const validateSignup = [
+  ...validateCredential,
+  body('name').notEmpty().withMessage('name is missing'),
+  body('email').isEmail().normalizeEmail().withMessage('invalid email'),
+  body('url')
+    .isURL()
+    .withMessage('invalid URL')
+    .optional({ nullable: true, checkFalsy: true }), // 옵션이니까 데이터가 없거나 텅텅빈 문자열이어도 허용해준다
+  validate,
+];
+
+router.post('/signup', validateSignup, authController.signup);
+
+router.post('/login', validateCredential, authController.login);
+
+router.get('/me', isAuth, authController.me);
+
+export default router;
