85dd25065e22c448f56268d5620d8c0ce1f314c2
lib26 <dladlsqja@naver.com>
2022-12-30 20:53:02 +0900

    client token

--- 13-dwitter-jwt/client/src/db/token.js
+++ 13-dwitter-jwt/client/src/db/token.js
@@ -2,6 +2,7 @@ const TOKEN = 'token';
 
 export default class TokenStorage {
   saveToken(token) {
+    // localStorage는 브라우저에서 쓸 수 있는 API
     localStorage.setItem(TOKEN, token);
   }
 
--- 13-dwitter-jwt/client/src/service/auth.js
+++ 13-dwitter-jwt/client/src/service/auth.js
@@ -15,7 +15,7 @@ export default class AuthService {
         url,
       }),
     });
-    this.tokenStorage.saveToken(data.token);
+    this.tokenStorage.saveToken(data.token); // 가입이 완료되면 토큰 저장
     return data;
   }
 
@@ -32,10 +32,11 @@ export default class AuthService {
     const token = this.tokenStorage.getToken();
     return this.http.fetch('/auth/me', {
       method: 'GET',
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: `Bearer ${token}` }, // 토큰 헤더에 추가
     });
   }
 
+  // 따로 서버에 로그아웃 처리 부탁 할 필요없이 토큰 지우면 그만이다
   async logout() {
     this.tokenStorage.clearToken();
   }
